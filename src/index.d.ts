f95f0e1208f6c7e4eb2a05ae71bff97d0baee318
boma <amakirisamuelboma@gmail.co>
2022-08-29 18:41:12 +0100

    fix module declearation

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,23 +1,25 @@
-declare module "react-native-progress-circle" {
-  import * as React from "react";
-  import * as ReactNative from "react-native";
+/** @format */
 
-  export interface PercentageCircleProps {
-    color?: string;
-    shadowColor?: string;
-    bgColor?: string;
-    radius: number;
-    borderWidth?: number;
-    percent: number;
-    children?: React.ReactNode;
-    containerStyle?: ReactNative.ViewStyle | Array<ReactNative.ViewStyle>;
-    outerCircleStyle?: ReactNative.ViewStyle | Array<ReactNative.ViewStyle>;
-  }
+declare module "progress-circle-react-native" {
+	import * as React from "react"
+	import * as ReactNative from "react-native"
 
-  export default class PercentageCircle extends React.Component<
-    PercentageCircleProps,
-    any
-  > {
-    render(): JSX.Element;
-  }
+	export interface PercentageCircleProps {
+		color?: string
+		shadowColor?: string
+		bgColor?: string
+		radius: number
+		borderWidth?: number
+		percent: number
+		children?: React.ReactNode
+		containerStyle?: ReactNative.ViewStyle | Array<ReactNative.ViewStyle>
+		outerCircleStyle?: ReactNative.ViewStyle | Array<ReactNative.ViewStyle>
+	}
+
+	export default class PercentageCircle extends React.Component<
+		PercentageCircleProps,
+		any
+	> {
+		render(): JSX.Element
+	}
 }
