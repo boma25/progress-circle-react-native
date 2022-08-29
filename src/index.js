b2268d25ec3683f4c319f35a6624d4f8df7afa71
boma <amakirisamuelboma@gmail.co>
2022-08-29 18:43:33 +0100

    take out duplicate proptypes decleration

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import React, { Component } from "react"
 import PropTypes from "prop-types"
 import { StyleSheet, View, I18nManager } from "react-native"
 import { ViewPropTypes } from "deprecated-react-native-prop-types"
-import PropTypes from "prop-types"
 
 // compatability for react-native versions < 0.44
 const ViewPropTypesStyle = ViewPropTypes
